ae7fa36af5296e253b0426d3864991d2b89ece02
Ekaterina <kh.katerina@bk.ru>
2021-06-25 11:19:59 +0300

    all

---
+++ Script-1.sql
@@ -0,0 +1,101 @@
+CREATE TABLE `like_post` (
+  `post_id` int(10) unsigned NOT NULL COMMENT 'ссылка на медиа',
+  `user_id` int(10) unsigned NOT NULL COMMENT 'Ссылка на пользователя, который поставил лайк',
+  `created_at` datetime DEFAULT current_timestamp() COMMENT 'Время создания строки',
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Время обновления строки'
+);
+
+CREATE TABLE `like_users` (
+  `user_id` int(10) unsigned NOT NULL COMMENT 'Ссылка на пользователя, который поставил лайк',
+  `created_at` datetime DEFAULT current_timestamp() COMMENT 'Время создания строки',
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Время обновления строки'
+);
+
+SELECT * FROM users LIMIT 10;
+UPDATE users SET updated_at = NOW() WHERE updated_at < created_at; 
+
+SELECT * FROM profiles LIMIT 10;
+UPDATE profiles SET photo_id = FLOOR(1 + RAND() * 100);
+UPDATE profiles SET status = FLOOR(1 + RAND() * 2);
+CREATE TEMPORARY TABLE genders (name CHAR(1));
+
+INSERT INTO genders VALUES ('m'), ('f'); 
+UPDATE profiles 
+  SET gender = (SELECT name FROM genders ORDER BY RAND() LIMIT 1);
+  SHOW TABLES;
+  
+ SELECT * FROM messages LIMIT 10;
+ UPDATE messages SET 
+  from_user_id = FLOOR(1 + RAND() * 100),
+  to_user_id = FLOOR(1 + RAND() * 100);
+  
+ SELECT * FROM media LIMIT 10;
+
+UPDATE media SET user_id = FLOOR(1 + RAND() * 100);
+UPDATE media SET media_type_id = FLOOR(1 + RAND() * 100);
+
+
+SELECT * FROM media_types;
+
+DELETE FROM media_types;
+
+INSERT INTO media_types (name) VALUES
+  ('photo'),
+  ('video'),
+  ('audio')
+;
+TRUNCATE media_types;
+SELECT * FROM media LIMIT 10;
+
+UPDATE media SET media_type_id = FLOOR(1 + RAND() * 3);
+UPDATE media SET user_id = FLOOR(1 + RAND() * 100);
+
+
+CREATE TEMPORARY TABLE extensions (name VARCHAR(10));
+
+INSERT INTO extensions VALUES ('jpeg'), ('avi'), ('mpeg'), ('png');
+
+SELECT * FROM extensions;
+
+UPDATE media SET filename = CONCAT(
+  'http://dropbox.net/vk/',
+  filename,
+  (SELECT last_name FROM users ORDER BY RAND() LIMIT 1),
+  '.',
+  (SELECT name FROM extensions ORDER BY RAND() LIMIT 1)
+);
+
+UPDATE media SET size = FLOOR(10000 + (RAND() * 1000000)) WHERE size < 1000;
+
+UPDATE media SET metadata = CONCAT('{"owner":"', 
+  (SELECT CONCAT(first_name, ' ', last_name) FROM users WHERE id = user_id),
+  '"}'); 
+  ALTER TABLE media MODIFY COLUMN metadata JSON;
+  
+ RENAME TABLE friendship TO friendships;
+ SELECT * FROM friendships LIMIT 10;
+ UPDATE friendships SET 
+  user_id = FLOOR(1 + RAND() * 100),
+  friend_id = FLOOR(1 + RAND() * 100);
+  UPDATE friendships SET friend_id = friend_id + 1 WHERE user_id = friend_id;
+  SELECT * FROM friendship_statuses;
+  
+ TRUNCATE friendship_statuses;
+ 
+INSERT INTO friendship_statuses (name) VALUES
+  ('Requested'),
+  ('Confirmed'),
+  ('Rejected');
+  
+ UPDATE friendships SET status_id = FLOOR(1 + RAND() * 3); 
+ SELECT * FROM communities;
+ DELETE FROM communities WHERE id > 20;
+ SELECT * FROM communities_users;
+ UPDATE communities_users SET community_id = FLOOR(1 + RAND() * 20);
+ 
+ SELECT * FROM like_media;
+ UPDATE like_media SET updated_at = NOW() WHERE updated_at < created_at; 
+ 
+ UPDATE like_post SET updated_at = NOW() WHERE updated_at < created_at; 
+ 
+ UPDATE like_users SET updated_at = NOW() WHERE updated_at < created_at; 
